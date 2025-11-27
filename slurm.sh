04f0676750af80593c8e528b397d40cba06f816e
Kian Hajireza <kian.hajireza@ri.se>
2025-11-27 16:09:31 +0100

    Running toy_fit

--- slurm.sh
+++ slurm.sh
@@ -27,9 +27,9 @@ source jax_env/bin/activate
 
 # proposed approach
 ## step 1: train MLP to fit nODE traj
-python train_ode.py --config configs/spin.yaml --target rober_fit
+#python train_ode.py --config configs/spin.yaml --target rober_fit
 # python train_ode.py --config configs/spin.yaml --target pollu_fit
-# python train_ode.py --config configs/spin.yaml --target toy_fit
+ python train_ode.py --config configs/spin.yaml --target toy_fit
 
 ## step 2: train CRNN with deriv from interpolated traj inferenced by MLP
 # python train_coll.py --config configs/spin.yaml --target rober_coll
